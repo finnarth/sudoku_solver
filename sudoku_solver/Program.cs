dda61e81efd86782bcf0fdc13a4f62874557c78e
FinnArth <finnarth@google.com>
2023-09-14 13:06:39 +0600

    2 Files were created:     Solver     SudokuCells

--- sudoku_solver/Program.cs
+++ sudoku_solver/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace sudoku_solver
+﻿using sudoku_solver.Tools;
+using System;
+
+namespace sudoku_solver
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+            Console.WriteLine("Sudoku solver made by FinnArth");
+            Console.WriteLine("Instruction: insert sudoku lines with 'x' instead of an empty cell");
+			Console.WriteLine("Your Sudoku:");
+
+			Solver.Input();
+			var solution = string .Join("\n", Solver.Solve());
+
+			Console.WriteLine();
+			Console.WriteLine("Solution:");
+			Console.Write(solution);
+			Console.WriteLine();
+			Console.WriteLine();
+        }
 	}
 }
